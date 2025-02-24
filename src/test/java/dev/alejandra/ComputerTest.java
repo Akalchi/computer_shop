65ea31ac0e6e6b74f1703e8b884313869d4b35fe
Akalchi <alejandrafdez1699@gmail.com>
2025-02-24 13:24:03 +0100

    "TEST [CreateComputer] failed"

---
+++ src/test/java/dev/alejandra/ComputerTest.java
@@ -0,0 +1,20 @@
+package dev.alejandra;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+public class ComputerTest {
+    @Test
+    void testCreateComputer() {
+        Computer computer = new Computer("HP Pavillion", 8, "Intel i7", "Windows 10", 800.0);
+
+        assertNotNull(computer);
+        assertEquals("HP Pavillion", computer.getBrand());
+        assertEquals(8, computer.getMemory());
+        assertEquals("Intel i7", computer.getProcessor());
+        assertEquals("Windows 10", computer.getOperatingSystem());
+        assertEquals(800.0, computer.getPrice());
+    }
+}
