a160dc1c8c10b43dd1afac09e3eb0cac995617c6
Akalchi <alejandrafdez1699@gmail.com>
2025-02-24 13:26:35 +0100

    "FEAT [Computer] implemented (attributes and getters)"

--- src/test/java/dev/alejandra/tests/ComputerTest.java
+++ src/test/java/dev/alejandra/tests/ComputerTest.java
@@ -1,9 +1,11 @@
-package dev.alejandra;
+package dev.alejandra.tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
+
+import dev.alejandra.model.Computer;
 
 public class ComputerTest {
     @Test
